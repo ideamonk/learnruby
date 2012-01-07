1785c063711cc614e0b1372c70300be036d375b6
Abhishek Mishra <ideamonk@gmail.com>
2012-01-07 15:52:02 +0530

    20/274

--- koans/about_objects.rb
+++ koans/about_objects.rb
@@ -27,30 +27,37 @@ class AboutObjects < EdgeCase::Koan
   def test_every_object_has_different_id
     obj = Object.new
     another_obj = Object.new
+    # well anything assigned as true, false, nil has the id of
+    # the respective objects assigned
+    # a=nil , b=a   // both a,b have object_id of 4
     assert_equal true, obj.object_id != another_obj.object_id
   end
 
   def test_some_system_objects_always_have_the_same_id
-    assert_equal __, false.object_id
-    assert_equal __, true.object_id
-    assert_equal __, nil.object_id
+    assert_equal 0, false.object_id
+    assert_equal 2, true.object_id
+    assert_equal 4, nil.object_id
   end
 
   def test_small_integers_have_fixed_ids
-    assert_equal __, 0.object_id
-    assert_equal __, 1.object_id
-    assert_equal __, 2.object_id
-    assert_equal __, 100.object_id
+    assert_equal 1, 0.object_id
+    assert_equal 3, 1.object_id
+    assert_equal 5, 2.object_id
+    assert_equal 201, 100.object_id
 
     # THINK ABOUT IT:
     # What pattern do the object IDs for small integers follow?
+    #
+    # Okay, object_id(n) =  n*2+1 where n is a number
+    # or bit-wise -
+    # object_id(n) = left_shift_with_1(n)
   end
 
   def test_clone_creates_a_different_object
     obj = Object.new
     copy = obj.clone
 
-    assert_equal __, obj           != copy
-    assert_equal __, obj.object_id != copy.object_id
+    assert_equal true, obj           != copy
+    assert_equal true, obj.object_id != copy.object_id
   end
 end
