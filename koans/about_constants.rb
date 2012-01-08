2900702cec9c76b1fa77e9b0dede9de1271ee835
Abhishek Mishra <ideamonk@gmail.com>
2012-01-08 08:58:59 +0530

    128/274

--- koans/about_constants.rb
+++ koans/about_constants.rb
@@ -7,16 +7,16 @@ class AboutConstants < EdgeCase::Koan
   C = "nested"
 
   def test_nested_constants_may_also_be_referenced_with_relative_paths
-    assert_equal __, C
+    assert_equal "nested", C
   end
 
   def test_top_level_constants_are_referenced_by_double_colons
-    assert_equal __, ::C
+    assert_equal "top level", ::C
   end
 
   def test_nested_constants_are_referenced_by_their_complete_path
-    assert_equal __, AboutConstants::C
-    assert_equal __, ::AboutConstants::C
+    assert_equal "nested", AboutConstants::C
+    assert_equal "nested", ::AboutConstants::C
   end
 
   # ------------------------------------------------------------------
@@ -35,7 +35,7 @@ class AboutConstants < EdgeCase::Koan
   end
 
   def test_nested_classes_inherit_constants_from_enclosing_classes
-    assert_equal __, Animal::NestedAnimal.new.legs_in_nested_animal
+    assert_equal 4, Animal::NestedAnimal.new.legs_in_nested_animal
   end
 
   # ------------------------------------------------------------------
@@ -47,7 +47,7 @@ class AboutConstants < EdgeCase::Koan
   end
 
   def test_subclasses_inherit_constants_from_parent_classes
-    assert_equal __, Reptile.new.legs_in_reptile
+    assert_equal 4, Reptile.new.legs_in_reptile
   end
 
   # ------------------------------------------------------------------
@@ -63,11 +63,14 @@ class AboutConstants < EdgeCase::Koan
   end
 
   def test_who_wins_with_both_nested_and_inherited_constants
-    assert_equal __, MyAnimals::Bird.new.legs_in_bird
+    assert_equal 2, MyAnimals::Bird.new.legs_in_bird
   end
 
   # QUESTION: Which has precedence: The constant in the lexical scope,
   # or the constant from the inheritance heirarachy?
+  # Lexical scope !
+  # Hmm I thought inheritance would happen at a later time and override
+  # the lexical scope though.
 
   # ------------------------------------------------------------------
 
@@ -78,10 +81,17 @@ class AboutConstants < EdgeCase::Koan
   end
 
   def test_who_wins_with_explicit_scoping_on_class_definition
-    assert_equal __, MyAnimals::Oyster.new.legs_in_oyster
+    assert_equal 4, MyAnimals::Oyster.new.legs_in_oyster
   end
 
   # QUESTION: Now Which has precedence: The constant in the lexical
   # scope, or the constant from the inheritance heirarachy?  Why is it
   # different than the previous answer?
+  # What I intuitively thought in previous koan turns unintuitively true
+  # for this one.
+  #
+  # inheritance wins here!  I don't get why is it different though, wouldn't one
+  # logically conclude the answer to be 2 from what we saw in previous koan?
+  # In name of simplicity, this is a bad idea because I've to remember this
+  # for later or avoid using nested classes at all for simplicity's sake :)
 end
